26c56e55a63595cbe7deb9399c6149429ff555d3
etv <tavareze2003@gmail.com>
2026-04-13 20:23:21 -0400

    Created ProductDto class

---
+++ src/main/java/com/tavarlabs/pos/dtos/ProductDto.java
@@ -0,0 +1,21 @@
+package com.tavarlabs.pos.dtos;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDto {
+    private String code;
+    private String name;
+    private int stock;
+    private double price;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
