9c0a9d962832ae936c35f23b2695333fa7b3c223
etv <tavareze2003@gmail.com>
2026-04-14 17:44:26 -0400

    created custom query to get invoices with their lines avoiding n+1 queries

--- src/main/java/com/tavarlabs/pos/repositories/InvoiceRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/InvoiceRepository.java
@@ -2,9 +2,17 @@ package com.tavarlabs.pos.repositories;
 
 import com.tavarlabs.pos.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    // If duplicated invoices show up at DISTINCT after the SELECT statement
+    @Query("SELECT inv FROM Invoice inv LEFT JOIN FETCH inv.lines")
+    List<Invoice> findAllWithLines();
+
+
 }
