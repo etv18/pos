f6e28aae5e4453c648c9884ad6704131d22497b7
etv <tavareze2003@gmail.com>
2026-04-14 12:56:53 -0400

    Added get product endpoint

--- src/main/java/com/tavarlabs/pos/controllers/ProductController.java
+++ src/main/java/com/tavarlabs/pos/controllers/ProductController.java
@@ -3,21 +3,26 @@ package com.tavarlabs.pos.controllers;
 import com.tavarlabs.pos.dtos.product.CreateProductRequest;
 import com.tavarlabs.pos.dtos.product.ProductDto;
 import com.tavarlabs.pos.dtos.product.UpdateProductRequest;
+import com.tavarlabs.pos.entity.Product;
+import com.tavarlabs.pos.mappers.ProductMapper;
 import com.tavarlabs.pos.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/products")
 @RestController
 public class ProductController {
     private final ProductService productService;
+    private final ProductMapper productMapper;
 
     @PostMapping
     public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody CreateProductRequest request){
-        ProductDto productDto = productService.createProduct(request);
+        ProductDto productDto = productMapper.toDto(productService.createProduct(request));
         return ResponseEntity.ok(productDto);
     }
 
@@ -26,7 +31,7 @@ public class ProductController {
             @PathVariable String code,
             @Valid @RequestBody UpdateProductRequest updateProductRequest
     ) {
-        ProductDto productDto = productService.updateProduct(code, updateProductRequest);
+        ProductDto productDto = productMapper.toDto(productService.updateProduct(code, updateProductRequest));
         return ResponseEntity.ok(productDto);
     }
 
@@ -34,5 +39,19 @@ public class ProductController {
     public ResponseEntity<Void> deleteProduct(@PathVariable String code) {
         productService.deleteProduct(code);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts(){
+        List<Product> products = productService.listProducts();
+        List<ProductDto> productsDto = products.stream().map(productMapper::toDto).toList();
+        return ResponseEntity.ok(productsDto);
+    }
+
+    @GetMapping(path = "/{code}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable String code){
+        Product product = productService.getProduct(code);
+        ProductDto productDto = productMapper.toDto(product);
+        return ResponseEntity.ok(productDto);
     }
 }
