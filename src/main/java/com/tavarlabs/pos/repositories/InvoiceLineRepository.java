131d57dc11d3cbbc08699e7fbfdffe5eaa2caeff
etv <tavareze2003@gmail.com>
2026-04-15 12:58:50 -0400

    created method to get an invoice with its line filtered by its code

--- src/main/java/com/tavarlabs/pos/repositories/InvoiceLineRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/InvoiceLineRepository.java
@@ -2,8 +2,15 @@ package com.tavarlabs.pos.repositories;
 
 import com.tavarlabs.pos.entity.InvoiceLine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InvoiceLineRepository extends JpaRepository<InvoiceLine, Long> {
+
+    @Query("SELECT il FROM InvoiceLine il WHERE il.invoice.code = :code")
+    List<InvoiceLine> findInvoiceLinesByInvoiceCode(@Param("code") String code);
 }
