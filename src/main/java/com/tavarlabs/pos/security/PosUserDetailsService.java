9d98dc28045a268bc4ca7565e55007ceb0a0820e
etv <tavareze2003@gmail.com>
2026-04-17 10:37:20 -0400

    Created PosUserDetailsService class implementing UserDetailsService interface

---
+++ src/main/java/com/tavarlabs/pos/security/PosUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.tavarlabs.pos.security;
+
+import com.tavarlabs.pos.entity.User;
+import com.tavarlabs.pos.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class PosUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+        return new PosUserDetails(user);
+    }
+}
