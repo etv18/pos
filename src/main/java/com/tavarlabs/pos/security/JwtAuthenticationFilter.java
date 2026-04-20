a164244c37b41f3a9f8c9cfa46b0d819c19273f7
etv <tavareze2003@gmail.com>
2026-04-20 11:09:21 -0400

    Set JwtAuthenticationFilter class

--- src/main/java/com/tavarlabs/pos/security/JwtAuthenticationFilter.java
+++ src/main/java/com/tavarlabs/pos/security/JwtAuthenticationFilter.java
@@ -1,14 +1,27 @@
 package com.tavarlabs.pos.security;
 
+import com.tavarlabs.pos.services.AuthenticationService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthenticationService authenticationService;
+    private static final String BEARER_ = "Bearer ";
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
@@ -16,6 +29,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     )
     throws ServletException, IOException {
+        try{
+            String token = extractToken(request);
+            if(token != null){
+                UserDetails userDetails = authenticationService.validateToken(token);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
 
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                if(userDetails instanceof PosUserDetails){
+                    request.setAttribute("userId", ((PosUserDetails) userDetails).getId());
+                }
+            }
+        } catch (Exception e) {
+            log.warn("Received invalid auth token");
+        }
+        filterChain.doFilter(request, response); //Important to add this cause it'll need for the next chain of filters
+    }
+
+    private String extractToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith(BEARER_))
+            return bearerToken.substring(BEARER_.length());
+        return null;
     }
 }
