98082e6b0326170d88467adf7d6e4e0e43681e07
etv <tavareze2003@gmail.com>
2026-04-20 11:11:12 -0400

    Created fragments/components template to set up thymeleaf fragments

--- src/main/java/com/tavarlabs/pos/config/SecurityConfig.java
+++ src/main/java/com/tavarlabs/pos/config/SecurityConfig.java
@@ -1,33 +1,91 @@
 package com.tavarlabs.pos.config;
 
+import com.tavarlabs.pos.entity.User;
+import com.tavarlabs.pos.repositories.UserRepository;
 import com.tavarlabs.pos.security.AuthEntryPoint;
+import com.tavarlabs.pos.security.JwtAuthenticationFilter;
+import com.tavarlabs.pos.security.PosUserDetailsService;
+import com.tavarlabs.pos.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthEntryPoint authorizeHandler;
+    @Bean
+    public AuthEntryPoint authorizeHandler(){
+        return new AuthEntryPoint();
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService){
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo){
+        PosUserDetailsService posUserDetailsService = new PosUserDetailsService(userRepo);
+        String username = "admin";
+        userRepo.findByUsername(username).orElseGet(() -> {
+            User user = User.builder()
+                    .fullName("User Admin")
+                    .username("admin")
+                    .password(passwordEncoder().encode("admin"))
+                    .roles(List.of())
+                    .build();
+            return userRepo.save(user);
+        });
+        return posUserDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        //Don't miss this bean, as is used in AuthenticationServiceImpl, spring won't start the program
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity httpSecurity,
+            JwtAuthenticationFilter jwtAuthenticationFilter
+    )
+            throws Exception
+    {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
                 .exceptionHandling(exception ->
-                    exception.authenticationEntryPoint(authorizeHandler)
+                    exception.authenticationEntryPoint(authorizeHandler())
                 )
                 .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests( auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/auth/test").authenticated()
                         .anyRequest().permitAll()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
