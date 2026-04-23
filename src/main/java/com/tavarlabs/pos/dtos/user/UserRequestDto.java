3c4edda00d605f8ed792427dafb73a1ad1d7bbad
etv <tavareze2003@gmail.com>
2026-04-22 23:33:53 -0400

    Created user dtos

---
+++ src/main/java/com/tavarlabs/pos/dtos/user/UserRequestDto.java
@@ -0,0 +1,21 @@
+package com.tavarlabs.pos.dtos.user;
+
+import com.tavarlabs.pos.entity.Role;
+import com.tavarlabs.pos.enums.RoleName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequestDto {
+    private String fullName;
+    private String username;
+    private String password;
+    private List<Role> roles;
+}
---
+++ src/main/java/com/tavarlabs/pos/dtos/user/UserResponseDto.java
@@ -0,0 +1,19 @@
+package com.tavarlabs.pos.dtos.user;
+
+import com.tavarlabs.pos.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponseDto {
+    private String fullName;
+    private String username;
+    private List<Role> roles;
+}
