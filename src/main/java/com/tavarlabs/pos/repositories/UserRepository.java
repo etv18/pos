e3f61084633d25c670e71566ca801c6a4ad34327
etv <tavareze2003@gmail.com>
2026-04-24 14:37:36 -0400

    Added method to find all users with its roles in just query.

--- src/main/java/com/tavarlabs/pos/repositories/UserRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/UserRepository.java
@@ -2,11 +2,16 @@ package com.tavarlabs.pos.repositories;
 
 import com.tavarlabs.pos.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles")
+    List<User> findAllWithRoles();
 }
