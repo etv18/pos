048f3d2a76417e11d67b64822daf401de3326104
etv <tavareze2003@gmail.com>
2026-04-23 20:59:28 -0400

    add findByRoleName method to RoleRepository interface

--- src/main/java/com/tavarlabs/pos/repositories/RoleRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.tavarlabs.pos.repositories;
 
 import com.tavarlabs.pos.entity.Role;
+import com.tavarlabs.pos.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(RoleName roleName);
 }
