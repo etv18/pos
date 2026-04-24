0e58ecd50e2edf73bafc28deb762f1347d810289
etv <tavareze2003@gmail.com>
2026-04-23 21:01:55 -0400

    Changed role list from List<Role> to List<String>

--- src/main/java/com/tavarlabs/pos/dtos/user/UserResponseDto.java
+++ src/main/java/com/tavarlabs/pos/dtos/user/UserResponseDto.java
@@ -1,5 +1,6 @@
 package com.tavarlabs.pos.dtos.user;
 
+import com.tavarlabs.pos.dtos.role.RoleDto;
 import com.tavarlabs.pos.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,5 +16,5 @@ import java.util.List;
 public class UserResponseDto {
     private String fullName;
     private String username;
-    private List<Role> roles;
+    private List<String> roles;
 }
