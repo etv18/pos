7eb23448c6eae5529ccdec1856e53b8ae7aaa4c2
etv <tavareze2003@gmail.com>
2026-04-23 21:03:37 -0400

    Refactored createUser method to be able to get the roles from the UserRequestDto obj and extract the accurate role objs from the database

--- src/main/java/com/tavarlabs/pos/services/impl/UserServiceImpl.java
+++ src/main/java/com/tavarlabs/pos/services/impl/UserServiceImpl.java
@@ -1,26 +1,33 @@
 package com.tavarlabs.pos.services.impl;
 
 import com.tavarlabs.pos.dtos.user.UserRequestDto;
+import com.tavarlabs.pos.entity.Role;
 import com.tavarlabs.pos.entity.User;
 import com.tavarlabs.pos.repositories.UserRepository;
+import com.tavarlabs.pos.services.RoleService;
 import com.tavarlabs.pos.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public User createUser(UserRequestDto userDto) {
+        List<Role> roles = roleService.mapRoles(userDto.getRoles());
+
         User user = User.builder()
                 .fullName(userDto.getFullName())
                 .username(userDto.getUsername())
                 .password(passwordEncoder.encode(userDto.getPassword()))
-                .roles(userDto.getRoles())
+                .roles(roles)
                 .build();
 
         return userRepository.save(user);
