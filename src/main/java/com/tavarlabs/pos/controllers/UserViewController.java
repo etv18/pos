b4868ece1cfdd5636cd8c77e6b053f646817131a
etv <tavareze2003@gmail.com>
2026-04-24 14:38:49 -0400

    Added a users list to model in object in create endpoint

--- src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
+++ src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
@@ -1,17 +1,28 @@
 package com.tavarlabs.pos.controllers;
 
+import com.tavarlabs.pos.mappers.UserMapper;
+import com.tavarlabs.pos.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UserViewController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
     @GetMapping("/create")
     public String createView(Model model){
         model.addAttribute("tabTitle", "User");
+        model.addAttribute(
+                "users",
+                userService.getAllUsers().stream()
+                        .map(user -> userMapper.toResponseDto(user))
+        );
         return "user/index";
     }
 }
