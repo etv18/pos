0a33cbd22898241f76dc5c9a1c7520e87b3d1b63
etv <tavareze2003@gmail.com>
2026-04-23 21:01:02 -0400

    Refactored UserMapper interface to be able to map roles to a list of strings

--- src/main/java/com/tavarlabs/pos/mappers/UserMapper.java
+++ src/main/java/com/tavarlabs/pos/mappers/UserMapper.java
@@ -1,11 +1,30 @@
 package com.tavarlabs.pos.mappers;
 
 import com.tavarlabs.pos.dtos.user.UserResponseDto;
+import com.tavarlabs.pos.entity.Role;
 import com.tavarlabs.pos.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import java.util.List;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface UserMapper {
+    @Mapping(target = "roles", source = ".", qualifiedByName = "mapToRolesToStrings")
     UserResponseDto toResponseDto(User user);
+
+    @Named("mapToRolesToStrings")
+    default List<String> mapToRolesToStrings(User user){
+        final String ROLE_ = "ROLE_";
+        return user.getRoles().stream()
+                .map(role -> {
+                    return role.getRoleName().name().replace(ROLE_, "");
+                })
+                .toList();
+    }
 }
