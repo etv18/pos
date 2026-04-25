e0f8c5387d246b1052e4c8102cd8f487db210fd9
etv <tavareze2003@gmail.com>
2026-04-25 16:55:58 -0400

    Set up UpdateUserRequestDto

---
+++ src/main/java/com/tavarlabs/pos/dtos/user/UpdateUserRequestDto.java
@@ -0,0 +1,36 @@
+package com.tavarlabs.pos.dtos.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserRequestDto {
+    @NotBlank(message = "User full name is required")
+    private String fullName;
+
+    @NotBlank(message = "User's username is required")
+    private String username;
+
+    @NotBlank(message = "Old user's username must be field")
+    private String oldUsername;
+
+    @NotBlank(message = "You must type a password")
+    private String password;
+
+    @NotNull(message = "You must assign at least one role to a user")
+    @Size(min = 1, message = "You must assign at least one role to a user")
+    private List<String> roles;
+
+
+    private boolean isActive;
+}
