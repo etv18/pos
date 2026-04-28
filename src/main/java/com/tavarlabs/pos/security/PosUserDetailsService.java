e05d62a7445302d27a246740f37bbc6ca735469b
etv <tavareze2003@gmail.com>
2026-04-28 19:48:17 -0400

    Changed from findByUsername to findByUsernameWithRoles method to load user and its roles all at once

--- src/main/java/com/tavarlabs/pos/security/PosUserDetailsService.java
+++ src/main/java/com/tavarlabs/pos/security/PosUserDetailsService.java
@@ -13,7 +13,7 @@ public class PosUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(
+        User user = userRepository.findByUsernameWithRoles(username).orElseThrow(
                 () -> new UsernameNotFoundException("User not found with username: " + username)
         );
         return new PosUserDetails(user);
