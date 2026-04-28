ccbdfd82d01b74d2de0427dd730c36edab80e68a
etv <tavareze2003@gmail.com>
2026-04-28 19:50:03 -0400

    Set up method findByUsernameWithRoles in user repository

--- src/main/java/com/tavarlabs/pos/repositories/UserRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     @Query("SELECT u FROM User u JOIN FETCH u.roles")
     List<User> findAllWithRoles();
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<User> findByUsernameWithRoles(String username);
 }
