c68c6caaaed2e55378ee1b8c7dd61288f3bbd437
etv <tavareze2003@gmail.com>
2026-04-29 13:00:05 -0400

    Create AuthViewController to render auth html templates

---
+++ src/main/java/com/tavarlabs/pos/controllers/AuthViewController.java
@@ -0,0 +1,19 @@
+package com.tavarlabs.pos.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(path = "/auth")
+public class AuthViewController {
+
+    @GetMapping("/login")
+    public String viewLogin(Model model){
+        return "/auth/login";
+    }
+}
