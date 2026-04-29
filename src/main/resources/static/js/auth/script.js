9e52bdbe713ebd3a5595cf424f7cd83bc4d27e99
etv <tavareze2003@gmail.com>
2026-04-29 13:01:00 -0400

    Js file to handle dynamic operations in login form

---
+++ src/main/resources/static/js/auth/script.js
@@ -0,0 +1,28 @@
+const btnSignIn = document.getElementById("btnSignIn");
+const loginForm = document.getElementById("loginForm");
+const txtUsername = document.getElementById("txtUsername");
+const txtPassword = document.getElementById("txtPassword");
+const loginEndpoint = "http://localhost:8080/api/v1/auth/login";
+
+async function logIn(username, password){
+    const res = await fetch(loginEndpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        alert(data.message);
+    }
+}
+
+btnSignIn.addEventListener("click", async e => {
+    await logIn(txtUsername.value, txtPassword.value);
+});
