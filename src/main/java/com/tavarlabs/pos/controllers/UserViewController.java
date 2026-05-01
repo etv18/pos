eee82875a83a4895879c01fef3273c1c4d40b729
etv <tavareze2003@gmail.com>
2026-04-30 23:40:29 -0400

    Added username property to model object of the template ui

--- src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
+++ src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
@@ -3,6 +3,8 @@ package com.tavarlabs.pos.controllers;
 import com.tavarlabs.pos.mappers.UserMapper;
 import com.tavarlabs.pos.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,9 @@ public class UserViewController {
     private final UserMapper userMapper;
 
     @GetMapping("/index")
-    public String createView(Model model){
+    public String createView(Model model, Authentication authentication){
+        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        model.addAttribute("username", username.toUpperCase());
         model.addAttribute("tabTitle", "User");
         model.addAttribute(
                 "users",
