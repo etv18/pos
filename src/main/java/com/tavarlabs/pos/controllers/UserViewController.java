b60275312addf57cae7d39f0e8ff4c476b7458a5
etv <tavareze2003@gmail.com>
2026-05-04 14:14:18 -0400

    Added roles model of user/index template

--- src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
+++ src/main/java/com/tavarlabs/pos/controllers/UserViewController.java
@@ -1,5 +1,7 @@
 package com.tavarlabs.pos.controllers;
 
+import com.tavarlabs.pos.enums.RoleName;
+import com.tavarlabs.pos.mappers.RoleMapper;
 import com.tavarlabs.pos.mappers.UserMapper;
 import com.tavarlabs.pos.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,18 +12,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UserViewController {
     private final UserService userService;
     private final UserMapper userMapper;
+    private final RoleMapper roleMapper;
+    final String ROLE_ = "ROLE_";
 
     @GetMapping("/index")
     public String createView(Model model, Authentication authentication){
         String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        List<String> roles = Arrays.asList(RoleName.values())
+                .stream()
+                .map(RoleName::name)
+                .map(roleNameStr -> roleNameStr.substring(ROLE_.length()))
+                .toList();
         model.addAttribute("username", username.toUpperCase());
         model.addAttribute("tabTitle", "User");
+        model.addAttribute("roles", roles);
         model.addAttribute(
                 "users",
                 userService.getAllUsers().stream()
