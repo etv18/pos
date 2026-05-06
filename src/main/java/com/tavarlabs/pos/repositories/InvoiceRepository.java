008c6df0be1b608b8e1c51fa952a4beb2a073fca
etv <tavareze2003@gmail.com>
2026-05-06 12:56:18 -0400

    Added findByCode method

--- src/main/java/com/tavarlabs/pos/repositories/InvoiceRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/InvoiceRepository.java
@@ -3,9 +3,11 @@ package com.tavarlabs.pos.repositories;
 import com.tavarlabs.pos.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -13,5 +15,8 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     // If duplicated invoices show up at DISTINCT after the SELECT statement
     @Query("SELECT inv FROM Invoice inv LEFT JOIN FETCH inv.lines")
     List<Invoice> findAllWithLines();
+
+    @Query("SELECT inv FROM Invoice inv JOIN FETCH inv.lines WHERE inv.code = :code")
+    Optional<Invoice> findByCode(@Param("code") String code);
     boolean existsByCode(String code);
 }
