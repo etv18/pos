0736eef73d00cbc7dcecf57654adb48a722e3ddc
etv <tavareze2003@gmail.com>
2026-05-07 12:29:17 -0400

    Created for purchase controller

---
+++ src/main/java/com/tavarlabs/pos/controllers/PurchaseController.java
@@ -0,0 +1,39 @@
+package com.tavarlabs.pos.controllers;
+
+import com.tavarlabs.pos.dtos.purchase.CreatePurchaseRequest;
+import com.tavarlabs.pos.dtos.purchase.PurchaseDto;
+import com.tavarlabs.pos.entity.Purchase;
+import com.tavarlabs.pos.mappers.PurchaseMapper;
+import com.tavarlabs.pos.services.PurchaseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/api/v1/purchases")
+public class PurchaseController {
+    /* TODO: replace purchase related objects with purchases */
+
+    private final PurchaseService purchaseService;
+    private final PurchaseMapper purchaseMapper;
+
+    @GetMapping
+    public ResponseEntity<List<PurchaseDto>> getAllPurchases(){
+        List<Purchase> purchases = purchaseService.getAllPurchases();
+        List<PurchaseDto> purchaseDtos = purchases.stream().map(purchaseMapper::toDto).toList();
+        return ResponseEntity.ok(purchaseDtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<PurchaseDto> createPurchase(
+            @Valid @RequestBody CreatePurchaseRequest createPurchaseRequest
+    ){
+        Purchase purchase = purchaseService.createPurchase(createPurchaseRequest);
+        PurchaseDto purchaseDto = purchaseMapper.toDto(purchase);
+        return ResponseEntity.ok(purchaseDto);
+    }
+}
