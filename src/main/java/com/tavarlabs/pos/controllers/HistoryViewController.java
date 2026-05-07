ae91241686fd8cdbf0cc6105ec48d5b0b8f26a52
etv <tavareze2003@gmail.com>
2026-05-07 12:30:44 -0400

    Renamed class

--- src/main/java/com/tavarlabs/pos/controllers/HistoryViewController.java
+++ src/main/java/com/tavarlabs/pos/controllers/HistoryViewController.java
@@ -2,8 +2,12 @@ package com.tavarlabs.pos.controllers;
 
 import com.tavarlabs.pos.dtos.invoice.InvoiceDto;
 import com.tavarlabs.pos.dtos.invoice.InvoiceWithoutLinesDto;
+import com.tavarlabs.pos.dtos.purchase.PurchaseDto;
+import com.tavarlabs.pos.dtos.purchase.PurchaseWithoutLinesDto;
 import com.tavarlabs.pos.mappers.InvoiceMapper;
+import com.tavarlabs.pos.mappers.PurchaseMapper;
 import com.tavarlabs.pos.services.InvoiceService;
+import com.tavarlabs.pos.services.PurchaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,10 +21,13 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping(path = "/sales")
-public class SalesViewController {
+@RequestMapping(path = "/history")
+public class HistoryViewController {
     private final InvoiceService invoiceService;
     private final InvoiceMapper invoiceMapper;
+
+    private final PurchaseService purchaseService;
+    private final PurchaseMapper purchaseMapper;
 
     @GetMapping(path = "/invoices")
     public String viewAllInvoices(Model model, Authentication authentication){
@@ -33,7 +40,7 @@ public class SalesViewController {
         model.addAttribute("invoices", invoiceWithoutLines);
         model.addAttribute("tabTitle", "Sales");
 
-        return "sales/index";
+        return "history/invoices";
     }
 
     @GetMapping(path = "/invoices/{invoiceCode}")
@@ -49,6 +56,36 @@ public class SalesViewController {
         model.addAttribute("invoice", invoiceDto);
         model.addAttribute("tabTitle", "Sales");
 
-        return "sales/invoiceDetails";
+        return "history/invoiceDetails";
+    }
+
+    @GetMapping(path = "/purchases")
+    public String viewAllPurchases(Model model, Authentication authentication){
+        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        List<PurchaseWithoutLinesDto> purchaseWithoutLines = purchaseService.getAllPurchases().stream()
+                .map(purchaseMapper::toPurchaseWithoutLinesDto)
+                .toList();
+
+        model.addAttribute("username", username.toUpperCase());
+        model.addAttribute("purchases", purchaseWithoutLines);
+        model.addAttribute("tabTitle", "Sales");
+
+        return "history/purchases";
+    }
+
+    @GetMapping(path = "/purchases/{purchaseCode}")
+    public String viewPurchaseDetails(
+            Model model,
+            Authentication authentication,
+            @PathVariable("purchaseCode") String code
+    ){
+        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        PurchaseDto purchaseDto = purchaseMapper.toDto(purchaseService.findPurchaseByCode(code));
+
+        model.addAttribute("username", username.toUpperCase());
+        model.addAttribute("purchase", purchaseDto);
+        model.addAttribute("tabTitle", "Sales");
+
+        return "history/purchaseDetails";
     }
 }
