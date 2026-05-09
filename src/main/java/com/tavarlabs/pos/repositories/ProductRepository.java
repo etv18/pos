9509c83954b8b8e1f1f5cf7e04e5012485fb5c63
etv <tavareze2003@gmail.com>
2026-05-08 21:51:13 -0400

    Added methods to count products based on certain conditions

--- src/main/java/com/tavarlabs/pos/repositories/ProductRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.tavarlabs.pos.repositories;
 
 import com.tavarlabs.pos.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,10 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByCode(String code);
     Optional<Product> findByCode(String code);
+
+    @Query("SELECT Count(p) FROM Product p WHERE p.stock < :minimumStock")
+    long countProductsWithLowStock(int minimumStock);
+
+    @Query("SELECT COUNT(p) FROM Product p WHERE p.stock < 1")
+    long countProductsOutOfStock();
 }
