2d82cde38e7ef3144f87a869242c605710c0b8e6
etv <tavareze2003@gmail.com>
2026-05-08 21:49:05 -0400

    Added total fields

--- src/main/java/com/tavarlabs/pos/entity/Invoice.java
+++ src/main/java/com/tavarlabs/pos/entity/Invoice.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CollectionId;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -35,6 +37,8 @@ public class Invoice {
     @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL)
     private List<InvoiceLine> lines;
 
+    private BigDecimal total;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
@@ -45,5 +49,20 @@ public class Invoice {
     protected void onCreate(){
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
+        calculateTotal();
+    }
+
+    public void calculateTotal() {
+        if(this.lines == null || this.lines.isEmpty()) {
+            this.total = BigDecimal.ZERO;
+        }
+
+        this.total = lines.stream().map(line -> {
+            double priceAsDouble = line.getProduct().getPrice();
+            BigDecimal price = BigDecimal.valueOf(priceAsDouble);
+            return price.multiply(BigDecimal.valueOf(line.getQuantity()));
+        })
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .setScale(2, RoundingMode.HALF_UP);
     }
 }
--- src/main/java/com/tavarlabs/pos/entity/InvoiceLine.java
+++ src/main/java/com/tavarlabs/pos/entity/InvoiceLine.java
@@ -3,6 +3,7 @@ package com.tavarlabs.pos.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -28,6 +29,8 @@ public class InvoiceLine {
     @Column(nullable = false)
     private int quantity;
 
+    private BigDecimal subTotal;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
@@ -38,5 +41,10 @@ public class InvoiceLine {
     protected void onCreate(){
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
+        calculateSubTotal();
+    }
+
+    public void calculateSubTotal(){
+        this.subTotal = BigDecimal.valueOf((quantity * product.getPrice()));
     }
 }
