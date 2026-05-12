5d643d4e4170ce806567f993732a917c33aa135d
etv <tavareze2003@gmail.com>
2026-05-11 21:57:11 -0400

    Set method setDataValuesOnDashboard in js file

--- src/main/resources/static/js/dashboard/script.js
+++ src/main/resources/static/js/dashboard/script.js
@@ -1,6 +1,42 @@
 const monthlySalesTotalCanvas = document.getElementById("monthlySalesTotalCanvas");
+const pAvailableItems = document.getElementById("pAvailableItems");
+const pRunningLowItems = document.getElementById("pRunningLowItems");
+const pOutOfStockItems = document.getElementById("pOutOfStockItems");
+const pTotalSalesToday = document.getElementById("pTotalSalesToday");
+const pSoldItems = document.getElementById("pSoldItems");
+
 const monthlySalesTotalInstance = null;
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const monthlySalesTotalsEndpoint = "/api/v1/stats/monthly/sales/totals";
+const dashboardDataEndpoint = "/api/v1/stats/dashboard/data";
+
+async function fetchBackendData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    if(!response.ok) {
+        console.error(data.message);
+        return null;
+    }
+    return data;
+}
+
+async function setDataValuesOnDashboard(){
+    const data = await fetchBackendData(dashboardDataEndpoint);
+    console.log(data);
+    pAvailableItems.textContent  = data.availableItems;
+    pRunningLowItems.textContent = data.runningLowItems;
+    pOutOfStockItems.textContent = data.outOfStockItems;
+    pTotalSalesToday.textContent = toCurrency(data.totalTodaySales);
+    pSoldItems.textContent       = data.soldItems;
+}
+
+function toCurrency(numberInStr){
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    return formatter.format(numberInStr);
+}
 
 function generateMonthlySalesTotalChart(chartInstance, canvas, type, data){
     if(chartInstance) chartInstance.destroy();
@@ -27,8 +63,7 @@ function generateMonthlySalesTotalChart(chartInstance, canvas, type, data){
 
 document.addEventListener("DOMContentLoaded", async e =>{
     const currentYear = new Date().getFullYear();
-    const response = await fetch(`/api/v1/stats/monthly/sales/totals/${currentYear}`);
-    const data = await response.json();
+    const data = await fetchBackendData(`${monthlySalesTotalsEndpoint}/${currentYear}`);
     console.log(data)
     const refactoredData = data.map(d => d.total);
     generateMonthlySalesTotalChart(
@@ -37,4 +72,6 @@ document.addEventListener("DOMContentLoaded", async e =>{
         "bar",
         refactoredData
     )
+
+    await setDataValuesOnDashboard();
 });
