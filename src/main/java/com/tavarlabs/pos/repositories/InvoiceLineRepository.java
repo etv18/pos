b1ff65cdef07b5c0164cff512ce8ca8a15f1021d
etv <tavareze2003@gmail.com>
2026-05-11 21:55:37 -0400

    Added countSoldItemsBasedOnTimeFrame method

--- src/main/java/com/tavarlabs/pos/repositories/InvoiceLineRepository.java
+++ src/main/java/com/tavarlabs/pos/repositories/InvoiceLineRepository.java
@@ -6,11 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InvoiceLineRepository extends JpaRepository<InvoiceLine, Long> {
 
     @Query("SELECT il FROM InvoiceLine il WHERE il.invoice.code = :code")
     List<InvoiceLine> findInvoiceLinesByInvoiceCode(@Param("code") String code);
+
+    @Query("SELECT SUM(il.quantity) FROM InvoiceLine il WHERE il.createdAt BETWEEN :start AND :end")
+    Optional<Long> countSoldItemsBasedOnTimeFrame(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
 }
