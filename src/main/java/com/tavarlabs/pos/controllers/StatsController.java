07bc9132bef8b265b2d6064f9a5c4b302c2ff893
etv <tavareze2003@gmail.com>
2026-05-11 21:57:41 -0400

    Added getDashboardData endpoint to StatsController

--- src/main/java/com/tavarlabs/pos/controllers/StatsController.java
+++ src/main/java/com/tavarlabs/pos/controllers/StatsController.java
@@ -1,14 +1,13 @@
 package com.tavarlabs.pos.controllers;
 
+import com.tavarlabs.pos.dtos.stats.DashboardData;
 import com.tavarlabs.pos.dtos.stats.MonthlySum;
 import com.tavarlabs.pos.services.StatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,6 +22,16 @@ public class StatsController {
     ){
         List<MonthlySum> result = statsService.monthlySalesTotals(year);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/dashboard/data")
+    public ResponseEntity<DashboardData> getDashboardData(
+            //This way spring will not throw an error if I don't give start and end values
+            @RequestParam(required = false) LocalDateTime start,
+            @RequestParam(required = false) LocalDateTime end
+    ){
+         DashboardData dd = statsService.getDashboardData(start, end);
+         return ResponseEntity.ok(dd);
     }
 
 }
